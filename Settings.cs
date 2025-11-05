38b3dea5e10c49f69efa28b53bb86bcce6dab523
Erik Emilov <erik2006de@gmail.com>
2025-11-05 23:31:49 +0200

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErikBank.Configuration
+{
+   /// <summary>
+   /// Project level configuration settings
+   /// </summary>
+    public class Settings
+    {
+        /// <summary>
+        /// Customer number starts from 1001;incremented by 1
+        /// </summary>
+        public static long BaseCustomerNo { get; set; } = 1000;
+    }
+}
