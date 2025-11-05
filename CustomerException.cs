0c9b2e21f5c9896c23e6bdcd8f71b913ccd979a4
Erik Emilov <erik2006de@gmail.com>
2025-11-05 23:33:59 +0200

    Add files via upload

---
+++ CustomerException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ErikBank.Exceptions
+{
+    /// <summary>
+    /// Exception class that represents error raised on Customer class
+    /// </summary>
+    public class CustomerException:ApplicationException
+    {
+        /// <summary>
+        /// Constructor that initializes exception messages
+        /// </summary>
+        /// <param name="message"></param>
+        public CustomerException(string message):base(message)
+        {
+
+        }
+        /// <summary>
+        /// Constructor that initializes exception message and inner exception
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public CustomerException(string message,Exception innerException):base(message,innerException)
+        {
+
+        }
+    }
+}
---
+++ ICustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ErikBank.Entities.Contracts
+{
+    /// <summary>
+    /// Represends interface of customer entity
+    /// </summary>
+    public interface ICustomer
+    {
+        #region Properties
+        Guid CustomerID { get; set; }
+        long CustomerCode { get; set; }
+        string CustomerName { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string Landmark { get; set; }
+        string Country { get; set; }
+        string Mobile { get; set; }
+        #endregion
+
+
+    }
+
+}
